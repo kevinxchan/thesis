192b5afc1c6fa10f461cef35fadcc1fd70335f4f
kevinxchan <kevchan1@alumni.ubc.ca>
2018-12-17 15:49:51 -0800

    add more descriptive logging messages; add docs

--- py/lca.py
+++ py/lca.py
@@ -58,13 +58,13 @@ def query_jgi_taxa(entry):
 	url = "http://taxonomy.jgi-psf.org/name/sc"
 	entry = validate_query(entry)
 	if query_jgi_error(entry):
-		print "WARNING: query sequence %s. assigning an empty LCA." % (entry)
+		print "[query_jgi_taxa] WARNING: query sequence %s. assigning an empty LCA." % (entry)
 		return []
 	r = requests.get(os.path.join(url, entry))
 	try:
 		response = r.json()
 		error = response.values()[0]
-		print "ERROR: query with %s, %s failed with response '%s'. exiting." % (entry, error)
+		print "[query_jgi_taxa] ERROR: query with %s, %s failed with response '%s'. exiting." % (entry, error)
 		sys.exit(1)
 	except ValueError:
 		response = r.text
@@ -74,7 +74,7 @@ def query_jgi_taxa(entry):
 		level = tax_list[i]
 		if len(level.split(":")) == 1:
 			rank = "nr%d" % no_rank_level
-			print "WARNING: taxonomic rank not found for level '%s' assigning a rank of '%s'." % (level, rank)
+			print "[query_jgi_taxa] WARNING: taxonomic rank not found for level '%s' assigning a rank of '%s'." % (level, rank)
 			tax_list[i] = rank + ":" + level
 			no_rank_level += 1
 	return tax_list
@@ -95,7 +95,7 @@ def query_jgi_lca(entry_1, entry_2):
 	entry_1 = validate_query(entry_1)
 	entry_2 = validate_query(entry_2)
 	if query_jgi_error(entry_1) or query_jgi_error(entry_2):
-		print "WARNING: reference sequence %s or %s not found in NCBI. assigning an empty LCA." % (entry_1, entry_2)
+		print "[query_jgi_lca] WARNING: reference sequence %s or %s not found in NCBI. assigning an empty LCA." % (entry_1, entry_2)
 		return []
 
 	query = ",".join([entry_1, entry_2])
@@ -103,7 +103,7 @@ def query_jgi_lca(entry_1, entry_2):
 	try:
 		response = r.json()
 		error = response.values()[0]
-		print "ERROR: query with %s, %s failed with response '%s'. exiting." % (entry_1, entry_2, error)
+		print "[query_jgi_lca] ERROR: query with %s, %s failed with response '%s'. exiting." % (entry_1, entry_2, error)
 		sys.exit(1)
 	except ValueError:
 		response = r.text
@@ -113,7 +113,7 @@ def query_jgi_lca(entry_1, entry_2):
 		level = tax_list[i]
 		if len(level.split(":")) == 1:
 			rank = "nr%d" % no_rank_level
-			print "WARNING: taxonomic rank not found for level '%s' assigning a rank of '%s'." % (level, rank)
+			print "[query_jgi_lca] WARNING: taxonomic rank not found for level '%s' assigning a rank of '%s'." % (level, rank)
 			tax_list[i] = rank + ":" + level
 			no_rank_level += 1
 	return tax_list
@@ -126,22 +126,27 @@ def get_longest_lca(dataset_to_lca):
 	@return dict, where keys = dataset id and values = deepest lca
 	"""
 	optimal_placements = {}
-	depth = -1
 	for _id in dataset_to_lca:
-		if _id == "DRR129261":
-			# depth = -1
-			optimal_placement = []
-			for ref_id in dataset_to_lca[_id]:
-				print ref_id
-				if len(dataset_to_lca[_id][ref_id]) > depth:
-					optimal_placement = dataset_to_lca[_id][ref_id]
-					depth = len(optimal_placement)
-			optimal_placements[_id] = optimal_placement
-	print "max depth: %d" % depth
-	print optimal_placements
+		depth = -1
+		optimal_placement = []
+		for ref_id in dataset_to_lca[_id]:
+			print ref_id
+			if len(dataset_to_lca[_id][ref_id]) > depth:
+				optimal_placement = dataset_to_lca[_id][ref_id]
+				depth = len(optimal_placement)
+		optimal_placements[_id] = optimal_placement
 	return optimal_placements
 
 def parse_sam_file(sam_file, ref_seqs):
+	"""
+	parses a sam file and retrieves information specific to:
+		1. parameters used to generate the sam file
+		2. the dataset name (unique id)
+		3. each reference aligned to, and the length of the alignment as a percentage
+
+	@return aligned_len_map a dictionary mapping reference ids to their alignments as percentages
+	@return sam_file_obj an object representation of the information from the sam file mentioned above
+	"""
 	aligned_len_map = defaultdict(list) # key: reference ID, values: list of percent length of read mapped to reference
 	sam_file_obj = SamFile()
 	with open(sam_file, "r") as infile:
@@ -161,7 +166,7 @@ def parse_sam_file(sam_file, ref_seqs):
 				try:
 					ref_id, cigar_string = data[2], data[5]
 				except:
-					print "WARNING: had a problem with %s, skipping..." % sam_file
+					print "[query_jgi_lca] WARNING: had a problem with %s, skipping..." % sam_file
 					continue
 				try:
 					ref_length = len(ref_seqs[ref_id].sequence)
@@ -169,7 +174,7 @@ def parse_sam_file(sam_file, ref_seqs):
 					percent_aligned = float(cigar_len) / ref_length * 100
 					aligned_len_map[ref_id].append(percent_aligned)
 				except KeyError:
-					print "ERROR: reference id %s found in SAM file but not in the reference FASTA. Did you pass in the correct reference file?" % ref_id
+					print "[query_jgi_lca] ERROR: reference id %s found in SAM file but not in the reference FASTA. Did you pass in the correct reference file?" % ref_id
 					sys.exit(1)
 	return aligned_len_map, sam_file_obj
 
@@ -218,40 +223,41 @@ def calculate_distance(full_ref_lineage, reads_aligned_p, optimal_placement):
 def main():
 	args = get_args()
 	dataset_to_lca = defaultdict(dict)
-	print "gathering reference taxonomies from reference file..."
+	print "[main] INFO: gathering reference taxonomies from reference file..."
 	ref_seq_map = build_ref_seq_map(args.reference_fasta)
-	print "getting LCAs between dataset organism and all references..."
+	print "[main] INFO: getting LCAs between dataset organism and all references..."
 	with open(args.dataset_taxonomies, "r") as f:
 		for line in f:
 			dataset_id, taxonomy = line.strip().split("\t")
 			start_time = time.time()
 			for ref_record in ref_seq_map.values()[0:50]:
 				dataset_to_lca[dataset_id][ref_record.id] = query_jgi_lca(taxonomy, ref_record.taxonomy)
-			print "done for dataset %s. time elapsed: %ds" % (dataset_id, time.time() - start_time)
-	print "getting optimal placements (deepest LCA) for each dataset..."
+			print "[main] INFO: done for dataset %s. time elapsed: %ds" % (dataset_id, time.time() - start_time)
+	print "[main] INFO: getting optimal placements (deepest LCA) for each dataset..."
 	optimal_placements = get_longest_lca(dataset_to_lca)
-	print "running through each sam file for each dataset..."
+	print "[main] INFO: running through each sam file for each dataset..."
 	all_sam_files = []
 	for item in list_dir_abs(args.sample_dir):
 		if os.path.isdir(item):
 			for sam_file in list_dir_abs(item):
-				if sam_file.endswith("_top_hits.sam"):
+				if sam_file.endswith("_top_hits.sam"): # TODO: may need to make this more flexible
 					all_sam_files.append(parse_sam_file(sam_file, ref_seq_map))
-	print "calculating distances between each reference aligned to and its lca..."
+	print "[main] INFO: calculating distances between each reference aligned to and its lca..."
 	for v in all_sam_files:
-		# TODO: get full lineages of each reference aligned to 
+		# get full lineages of each reference aligned to 
 		# calculate distance between that and lca
 		# multiply by number of reads >= 80% (aligned_len_map in sam_obj)
 		aligned_len_map, sam_obj = v[0], v[1]
 		for ref_id in aligned_len_map:
 			full_ref_lineage = query_jgi_taxa(ref_seq_map[ref_id].taxonomy)
 			print "###########################################################"
-			print "full ref aligned to is lineage is: %s" % full_ref_lineage
-			print "optimal placement is: %s" % optimal_placements[sam_obj.dataset_name]
+			print "[main] INFO: full ref aligned to is lineage is: %s" % full_ref_lineage
+			print "[main] INFO: optimal placement is: %s" % optimal_placements[sam_obj.dataset_name]
 			print optimal_placements.keys()
 			distance = calculate_distance(full_ref_lineage, aligned_len_map[ref_id], optimal_placements[sam_obj.dataset_name])
-			print "DISTANCE IS: %d" % distance
+			print "[main] INFO: DISTANCE IS: %d" % distance
 			print "###########################################################"
+		# TODO: write out to file!
 
 if __name__ == "__main__":
 	main()
