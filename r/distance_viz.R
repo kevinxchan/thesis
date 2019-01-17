2efa064f5618d6547fe06f0652d3ccb61a73bf89
Kevin Chan <kevchan1@alumni.ubc.ca>
2019-01-17 10:12:15 -0800

    add connors code for second figure

--- r/distance_viz.R
+++ r/distance_viz.R
@@ -4,15 +4,6 @@ library(RColorBrewer)
 library(dplyr)
 library(optparse)
 
-#########
-# TODO: #
-#########
-# 1. after lca.py finished, remake the data frame and rename the columns appropriately
-# 2. figure 1 = real taxonomic distance vs proportion
-# 3. figure 2 = cumulative taxonomic distance vs marker gene. when comparing multiple markers, need to write
-#    more code to parse through a directory full of these dataframes and merge them
-# 4. instead of dividing by all totals, group by query dataset too and then divide by this total count.
-
 option_list = list(make_option(c("-i", "--input_directory"), type="character", default=NULL,
                                help="A directory containing tab-separated value files output by lca.py", metavar="character"),
                    make_option(c("-p", "--prefix"), type="character", default=".",
@@ -30,11 +21,12 @@ if (is.null(opt$input_directory)){
 # For debugging
 ##
 # prefix <- "viz"
-# input_directory <- "/Users/kevinxchan/Documents/UBC/YEAR4/MICB449/data/marker_gene_data/"
+# input_directory <- "/Users/kevinxchan/Documents/UBC/YEAR4/MICB449/data/marker_gene_data/with_lengths/"
 # opt <- data.frame(input_directory, prefix, stringsAsFactors = FALSE)
 
 spec_out <- file.path(opt$prefix, "Proportion_TaxDistance.png")
 pdist_out <- file.path(opt$prefix, "WeightedDistance_MarkerGene.png")
+cdist_out <- file.path(opt$prefix, "WeightedDistance_Dataset.png")
 
 # start building up acc_dat by scanning through files in directory
 file_names <- list.files(path = opt$input_directory)
@@ -58,7 +50,7 @@ for (file in file_names) {
 
 acc_header <- c("MarkerGene", "DatasetID", "ReferenceID", "Software", "ReferenceTaxonomy", "MeanAlignmentPercentage", "TotalReadsAligned", "NumReadsAlignedOver80", "PercReadsAlignedOver80", "TaxDistance", "CumDistance")
 names(acc_dat) <- acc_header
-acc_dat$Software <- sub("^(PN:)(.*)", "\\2", acc_dat$Software)
+acc_dat$Software <- sub("graphmap", "GraphMap", acc_dat$Software)
 
 ##
 # Figure 1: Rank-exclusion specificity in relation to optimal placement distance
@@ -66,8 +58,6 @@ acc_dat$Software <- sub("^(PN:)(.*)", "\\2", acc_dat$Software)
 sum_software <- acc_dat %>%
   group_by(MarkerGene, Software) %>%
   summarise(TotalBySoftware = sum(TotalReadsAligned, na.rm = T))
-
-nrow(acc_dat %>% filter(MeanAlignmentPercentage < 20))
 
 fig_1 <- acc_dat %>%
   filter(MeanAlignmentPercentage >= 20) %>%
@@ -100,48 +90,51 @@ ggsave(filename = spec_out, width = 10, height = 6, dpi = 400)
 # Figure 2: The taxonomic-hazard-weighted classification scores
 ##
 
-harm_dist_dat <- acc_dat %>% 
-  filter(Proportion > 0) %>% 
-  merge(build_params, by.x = "Marker", by.y = "name") %>% 
-  mutate(Marker = reorder(Marker, ref_sequences)) %>% 
-  mutate(Rank = reorder(Rank, Position)) %>%
-  mutate(PlaceDist = Distance*Proportion) %>% 
-  group_by(Trial, Software, Marker, Rank, ref_sequences) %>% 
-  summarise_at(vars(PlaceDist), funs(sum))
+# Determine the number of reads that were aligned by each tool for each dataset and marker gene
+fig3_dat <- group_by(acc_dat, Software, DatasetID, MarkerGene) %>% 
+  summarise_at(vars(TotalReadsAligned), funs(sum)) %>% 
+  rename(ReadPool = TotalReadsAligned)
 
-# TODO: break down by dataset
-sum_software_by_dataset <- acc_dat %>%
-  group_by(MarkerGene, DatasetID, Software) %>%
-  summarise(TotalBySoftwareDataset = sum(TotalReadsAligned, na.rm = T))
+harm_dist_dat <- fig3_dat %>% 
+  merge(acc_dat, by = c("Software", "DatasetID", "MarkerGene")) %>% 
+  mutate(Proportion = TotalReadsAligned/ReadPool) %>%
+  mutate(CumTaxDistance = TaxDistance*Proportion) %>% 
+  group_by(DatasetID, Software, MarkerGene) %>% 
+  summarise_at(vars(CumTaxDistance), funs(sum))
 
-fig_3 <- acc_dat %>%
-  select("MarkerGene", "DatasetID", "Software", "TotalReadsAligned", "TaxDistance", "CumDistance") #%>%
-  # group_by(MarkerGene, DatasetID, Software, CumDistance) %>%
-  # summarise(Total = sum(TotalReadsAligned)) %>%
-  # filter(TaxDistance >= 0)
-
-tmp <- fig_1 %>%
-  group_by(MarkerGene, DatasetID, Software) %>%
-  summarise(TotalBySoftwareDataset = sum(Total))
-
-tmp <- merge(tmp, fig_1, by = c("MarkerGene", "DatasetID", "Software"))
-fig_1 <- mutate(tmp, Proportion = Total / TotalBySoftwareDataset * 100)
-
-ggplot(fig_3, aes(x=MarkerGene, y=CumDistance)) +
-  geom_point(aes(fill=MarkerGene), colour="black",
-             shape=21, size=3, alpha=2/3) +
+ggplot(harm_dist_dat, aes(x=DatasetID, y=CumTaxDistance)) +
+  geom_boxplot() +
   scale_fill_brewer(palette = "PuOr") +
   facet_wrap(~Software) +
   ylab("Cumulative Taxonomic Distance") +
+  xlab("Dataset ID") +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_line(colour = "#bdbdbd", linetype = "dotted"),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave(filename = cdist_out, width = 8, height = 5, dpi = 400)
+
+ggplot(harm_dist_dat, aes(x=MarkerGene, y=CumTaxDistance)) +
+  geom_jitter(aes(fill=DatasetID), position=position_jitter(0.1), colour="black",
+              shape=21, size=3, alpha=2/3) +
+  # geom_point(aes(fill=DatasetID), colour="black",
+  #            shape=21, size=3, alpha=2/3) +
+  scale_fill_brewer(palette = "PuOr") +
+  facet_wrap(~Software) +
+  ylab("Cumulative Taxonomic Distance") +
+  xlab("Marker Gene") +
   theme(panel.background = element_blank(),
         panel.grid.major = element_line(colour = "#bdbdbd", linetype = "dotted"),
         panel.grid.minor = element_blank(),
         axis.text.x = element_text(angle = 45, hjust = 1))
 ggsave(filename = pdist_out, width = 8, height = 5, dpi = 400)
 
-harm_dist_dat %>%
-  group_by(Trial) %>% 
-  summarise_at(vars(PlaceDist), funs(mean, median))
+stat <- harm_dist_dat %>%
+  filter(!is.na(CumTaxDistance)) %>% 
+  group_by(Software) %>% 
+  summarise_at(vars(CumTaxDistance), funs(mean, median, var))
+
+stat
 
 ##
 # Welch Two Sample t-test between the two trials
