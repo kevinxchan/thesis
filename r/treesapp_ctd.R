c0ffbdb996b4c30ce1ed1df6a0007eda244232a9
Kevin Chan <kevchan1@alumni.ubc.ca>
2019-03-07 10:08:12 -0800

    prep script for CTD with TreeSAPP

---
+++ r/treesapp_ctd.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(RColorBrewer)
+
+raw_dat <- read_tsv("data/ctd_scores.txt")
+head(raw_dat)
+
+dat <- mutate_if(raw_dat, is.character, str_replace_all, pattern = "rL10P", replacement = "RPL10")
+
+ggplot(dat, aes(x = Marker, y = CTD_score)) +
+  geom_jitter(aes(fill = Dataset), shape = 21, size = 3, alpha = 2/3, position = position_jitter(0.15)) +
+  scale_fill_brewer(palette = "PuOr") +
+  xlab("Marker Gene") +
+  ylab("Cumulative Taxonomic Distance") +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_line(colour = "#bdbdbd", linetype = "dotted"),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+ 
