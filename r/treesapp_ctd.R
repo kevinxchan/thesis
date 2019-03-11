fea5cc6b3321a779c81f0c9fc7b17721e1e64242
Kevin Chan <kevchan1@alumni.ubc.ca>
2019-03-11 11:41:25 -0700

    add titles; save figure to disk

--- r/treesapp_ctd.R
+++ r/treesapp_ctd.R
@@ -6,13 +6,17 @@ head(raw_dat)
 
 dat <- mutate_if(raw_dat, is.character, str_replace_all, pattern = "rL10P", replacement = "RPL10")
 
-ggplot(dat, aes(x = Marker, y = CTD_score)) +
+fig <- ggplot(dat, aes(x = Marker, y = CTD_score)) +
   geom_jitter(aes(fill = Dataset), shape = 21, size = 3, alpha = 2/3, position = position_jitter(0.15)) +
   scale_fill_brewer(palette = "PuOr") +
   xlab("Marker Gene") +
   ylab("Cumulative Taxonomic Distance") +
+  ggtitle("CTD Per Marker Gene") +
   theme(panel.background = element_blank(),
         panel.grid.major = element_line(colour = "#bdbdbd", linetype = "dotted"),
         panel.grid.minor = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1))
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5))
+
+ggsave(fig, filename = "treesapp_ctd.png")
  
