5f13e16f06a153964ccffb8d11dbd462431dd96e
Kevin Chan <kevchan1@alumni.ubc.ca>
2019-04-02 16:07:59 -0700

    add new table

--- r/table_figures.R
+++ r/table_figures.R
@@ -34,3 +34,17 @@ g2 <- tableGrob(t2)
 h <- convertHeight(sum(g2$heights), "in", TRUE)
 w <- convertWidth(sum(g2$widths), "in", TRUE)
 ggsave(g2, filename = "dataset_metadata.png", width=w, height=h)
+
+## TABLE 3: comparison of sanger vs illumina vs nanopore reads
+sanger <- c(">800 bp", "Template using pool of molecules", "$0.00125 / bp", "Highest quality, relatively low throughput")
+illumina <- c("2x150 bp", "Template clonally derived from single molecule", "$6.67 x 10^-7 / bp", "High quality, extremely high throughput")
+nanopore <- c(">6-8 kbp", "Template is single nucleic acid sequence", "$4.75 x 10^-9 / bp", "Poor quality, comparitively low throughput")
+
+t3 <- tibble(sanger, illumina, nanopore)
+colnames(t3) <- c("Sanger reads", "Illumina reads", "Oxford Nanopore reads")
+
+g3 <- tableGrob(t3)
+h <- convertHeight(sum(g3$heights), "in", TRUE)
+w <- convertWidth(sum(g3$widths), "in", TRUE)
+ggsave(g3, filename = "comparison_sequence_tech.png", width=w, height=h, dpi=400)
+
